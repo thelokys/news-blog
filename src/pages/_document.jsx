77b6c002d0cda6c031acdc260281ad14acfdff81
thelokys <ricardo.guidetti@hotmail.com>
2020-12-06 22:29:39 -0300

    Added Example nextjs

---
+++ src/pages/_document.jsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import SEO from '../components/SEO';
+
+export default function Home() {
+  return (
+    <div>
+      <SEO 
+        image="teste.png" 
+        description="Descrição total" 
+        title="Titulo da página"
+      />
+      Hello World
+    </div>
+  );
+}
